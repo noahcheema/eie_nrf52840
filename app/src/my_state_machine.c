e0299e7e8d6437c66a0b7e997ef4ab7460cc2fbd
Noah Cheema <nawfalnoah@gmail.com>
2025-10-23 20:23:57 -0600

    Lesson 5 Exercise Part 1

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -9,11 +9,12 @@
 #include "my_state_machine.h"
 
 
-static void led_on_state_entry(void* o);
+
 static enum smf_state_result led_on_state_run(void* o);
-static void led_off_state_entry(void* o);
 static enum smf_state_result led_off_state_run(void* o);
 
+static void led_on_state_exit(void* o);
+static void led_off_state_exit(void* o);
 
 enum led_state_machine_states{
     LED_ON_STATE,
@@ -27,8 +28,8 @@ typedef struct {
 } led_state_object_t;
 
 static const struct smf_state led_states[] = {
-    [LED_ON_STATE] = SMF_CREATE_STATE(led_on_state_entry, led_on_state_run, NULL, NULL, NULL),
-    [LED_OFF_STATE] = SMF_CREATE_STATE(led_off_state_entry, led_off_state_run, NULL, NULL, NULL)
+    [LED_ON_STATE] = SMF_CREATE_STATE(NULL, led_on_state_run, led_on_state_exit, NULL, NULL),
+    [LED_OFF_STATE] = SMF_CREATE_STATE(NULL, led_off_state_run, led_off_state_exit, NULL, NULL)
 };
 
 static led_state_object_t led_state_object;
@@ -44,12 +45,12 @@ int state_machine_run(){
     return smf_run_state(SMF_CTX(&led_state_object));
 }
 
-static void led_on_state_entry(void *o){
-    LED_set(LED0, LED_ON);
+static void led_on_state_exit(void *o){
+    LED_set(LED0, LED_OFF);
 }
 
 static enum smf_state_result led_on_state_run(void *o){
-    if(led_state_object.count > 500){
+    if(led_state_object.count >100){
         led_state_object.count = 0;
         smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_OFF_STATE]);
     } else{
@@ -59,12 +60,12 @@ static enum smf_state_result led_on_state_run(void *o){
     return SMF_EVENT_HANDLED;
 }
 
-static void led_off_state_entry(void* o){
-    LED_set(LED0, LED_OFF);
+static void led_off_state_exit(void *o){
+    LED_set(LED0, LED_ON);
 }
 
 static enum smf_state_result led_off_state_run(void* o){
-    if(led_state_object.count>500){
+    if(led_state_object.count>100){
         led_state_object.count = 0;
         smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
     } else{
